75e1a068b03b7aff3bebf3d205019a780a4bbcda
Hector Sanchez <hsanchez@driveformvt.com>
2025-03-03 09:27:15 -0700

    Strealine the File Structure

--- src/routes.js
+++ src/routes.js
@@ -1,12 +1,3 @@
-import Index1 from "./pages/Index1/Index1";
-import Index2 from "./pages/Index2/Index2";
-import Index3 from "./pages/Index3/Index3";
-import Index4 from "./pages/Index4/Index4";
-import Index5 from "./pages/Index5/Index5";
-import Index6 from "./pages/Index6/Index6";
-import Index7 from "./pages/Index7/Index7";
-import Index8 from "./pages/Index8/Index8";
-import Index9 from "./pages/Index9/Index9";
 import RecruitmentLanding from "./pages/RecruitmentLanding/RecruitmentLanding";
 import Login from "./pages/Auth/login";
 import SignUp from "./pages/Auth/signup";
@@ -49,15 +40,6 @@ const routes = [
   },
 
   // Public routes
-  { path: "/index9", component: <Index9 /> },
-  { path: "/index8", component: <Index8 /> },
-  { path: "/index7", component: <Index7 /> },
-  { path: "/index6", component: <Index6 /> },
-  { path: "/index5", component: <Index5 /> },
-  { path: "/index4", component: <Index4 /> },
-  { path: "/index3", component: <Index3 /> },
-  { path: "/index2", component: <Index2 /> },
-  { path: "/index1", component: <Index1 /> },
   { path: "/", component: <RecruitmentLanding /> },
 ];
 
